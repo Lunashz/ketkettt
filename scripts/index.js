be995303502d2c27ab039f72e2b7f44fdd6c2840
Lunashz <95840606+Lunashz@users.noreply.github.com>
2026-09-06 20:46:36 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Sep 22, 2024',
+  birthdate: 'Sep 22, 2026',
   name: 'Aisha Keith'
 };
 
